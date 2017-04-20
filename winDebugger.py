4cf3fa2066b8cf587c355cb00932de8c5403eec7
mike <b09780978@gmail.com>
2017-04-20 22:28:46 +0800

    make this debug can attach a process(type in pid)

--- winDebugger.py
+++ winDebugger.py
@@ -6,8 +6,12 @@ kernel32 = windll.kernel32
 class debugger():
     
     def __init__(self):
-        pass
+        self.__h_process = None
+        self.__pid = None
+        self.__debugger_active = False
 
+    # load a file path, use CreateProcessA function to open it
+    # and use another process to debug it
     def load(self, file_path):
         # set create process flag to a debug process
         # this can receive all debug event from WaitForDebugEvent function
@@ -36,7 +40,53 @@ class debugger():
                                     ):
             print "[+] Successfully launch the process!"
             print "[+] PID : %d" % process_infomation.dwProcessId
+            # open a process for debug
+            self.__h_process = self.open_process(process_infomation.dwProcessId)
 
         else:
             print "[-] Fail!"
             print "[-] Error: 0x%08x" % kernel32.GetLastError()
+
+    # call OpenProcess function to generate a debug process(with all access)
+    def open_process(self, pid):
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+        return h_process
+
+    # open a debug process and attach the file's pid
+    def attach(self, pid):
+        self.__h_process = self.open_process(pid)
+        # try to attach file
+        if kernel32.DebugActiveProcess(pid):
+            self.__debugger_active = True
+            self.__pid = int(pid)
+            self.run()
+
+        else:
+            print "[-] Unable to attach process"
+
+    # run and wait for accept debug event
+    def run(self):
+        while self.__debugger_active:
+            self.get_debug_event()
+
+    # use a DEBUG_EVENT to catch all event
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            raw_input("Press a key to continue...")
+            self.__debugger_active = False
+            kernel32.ContinueDebugEvent(debug_event.dwProcessId,
+                                        debug_event.dwThreadId,
+                                        continue_status)
+
+    def detach(self):
+        if kernel32.DebugActiveProcessStop(self.__pid):
+            print "[+] Finished debugging."
+            print "[+] Exiting..."
+            return True
+        else:
+            print "[-] detach fail"
+            print "[-] Error: %0x%08x" % kernel32.GetLastError()
+            return False
--- winStructure.py
+++ winStructure.py
@@ -6,6 +6,8 @@ DWORD   =   c_ulong
 LPBYTE  =   POINTER(c_ubyte)
 LPTSTR  =   POINTER(c_char)
 HANDLE  =   c_void_p
+PVOID   =   c_void_p
+UINT_PTR=   c_ulong
 
 # define windows flags contents
 
@@ -17,6 +19,15 @@ STARTF_USESHOWWINDOW     =       0x00000001
 
 # CmdShow Flags
 SW_HIDE                  =       0x00000000
+
+# Process Access Flags
+PROCESS_ALL_ACCESS = 0x001f0fff
+
+# Debug continue Flags
+DBG_CONTINUE = 0x00010002
+
+# WaitForDebugEvent time
+INFINITE = 0xffffffff
 
 
 # define windows data structure
@@ -51,3 +62,59 @@ class PROCESS_INFORMATION(Structure):
         ("dwProcessId"      ,        DWORD),
         ("dwThreadId"       ,        DWORD)
         ]
+
+# because EXCEPTION_RECORD need to define self pointer
+# _fields_ need to fill latter
+class EXCEPTION_RECORD(Structure):
+    pass
+
+
+EXCEPTION_RECORD._fields_ = [
+    ("ExceptionCode"    ,        DWORD),
+    ("ExceptionFlags"   ,        DWORD),
+    ("ExceptionRecord"  ,   POINTER(EXCEPTION_RECORD)),
+    ("ExceptionAddress" ,           PVOID),
+    ("NumberParameters" ,        DWORD),
+    ("ExceptionInformatin",     UINT_PTR * 15)
+    ]
+
+#class EXCEPTION_RECORD(Structure):
+#    _fields_ = [
+#    ("ExceptionCode"    ,        DWORD),
+#    ("ExceptionFlags"   ,        DWORD),
+#    ("ExceptionRecord"  ,   POINTER(EXCEPTION_RECORD),
+#    ("ExceptionAddress" ,           PVOID),
+#    ("NumberParameters" ,        DWORD),
+#    ("ExceptionInformatin",     UINT_PTR * 15)
+#    ]
+    
+class EXCEPTION_DEBUG_INFO(Structure):
+    _fields_ = [
+        ("ExceptionRecord"  ,   EXCEPTION_RECORD),
+        ("dwFirstChance"    ,              DWORD)
+        ]
+
+# this union has many structure
+# but in this book only use exception
+class DEBUG_EVENT_UNION(Structure):
+    _fields_ = [
+        ("Exception"        ,       EXCEPTION_DEBUG_INFO),
+       # ("CreateThread"     ,       CREATE_THREAD_DEBUG_INFO),
+       # ("CreateProcessInfo",       CREATE_PROCESS_DEBUG_INFO),
+       # ("ExitThread"       ,       EXIT_THREAD_DEBUG_INFO),
+       # ("ExitProcess"      ,       EXIT_PROCESS_DEBUG_INFO),
+       # ("LoadDll"          ,       LOAD_DLL_DEBUG_INFO),
+       # ("UNLOADll"         ,       UNLOAD_DLL_DEBUG_INFO),
+       # ("DebugString"      ,       OUTPUT_DEBUG_STRING_INFO),
+       # ("RipInfo"          ,       RIP_INFO)
+        ]
+
+class DEBUG_EVENT(Structure):
+    _fields_ = [
+        ("dwDebugEnentCode"  ,       DWORD),
+        ("dwProcessId"       ,       DWORD),
+        ("dwThreadId"        ,       DWORD),
+        ("u"                 ,DEBUG_EVENT_UNION) 
+        ]
+
+
